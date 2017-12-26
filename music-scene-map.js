c42df04d328c1517424d62fa8679534ab04baa7d
Peter Oliver <git@mavit.org.uk>
2017-12-26 15:07:17 +0000

    Rename to music-scene-map.

--- music-scene-map.js
+++ music-scene-map.js
@@ -1,19 +1,19 @@
 // Copyright 2013 Peter Oliver.
 
-// This file is part of similar-artist-locations
+// This file is part of music-scene-map
 
-// similar-artist-locations is free software: you can redistribute it
+// music-scene-map is free software: you can redistribute it
 // and/or modify it under the terms of the GNU General Public
 // License as published by the Free Software Foundation, either
 // version 3 of the License, or (at your option) any later version.
 
-// similar-artist-locations is distributed in the hope that it will be
+// music-scene-map is distributed in the hope that it will be
 // useful, but WITHOUT ANY WARRANTY; without even the implied
 // warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with similar-artist-locations.  If not, see
+// along with music-scene-map.  If not, see
 // <https://www.gnu.org/licenses/>.
 
 function resize () {
